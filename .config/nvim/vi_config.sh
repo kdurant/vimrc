c08e92098c419c8d96f2ae52b003a81370264b71
kdurant <wangjun850725@163.com>
2019-12-03 10:44:39 +0800

    update vi_config.sh

--- .config/nvim/vi_config.sh
+++ .config/nvim/vi_config.sh
@@ -17,16 +17,17 @@ if [ ! -d $HOME/.config/nvim ]; then
 fi
 
 echo "--------------------复制vim配置文件-----------------------"
-cp $HOME/vimrc/.config/nvim/init.vim 		        ~/.config/nvim
-cp $HOME/vimrc/.config/nvim/basic_map.vim 	        ~/.config/nvim
-cp $HOME/vimrc/.config/nvim/plugin_config.vim 		~/.config/nvim
-cp $HOME/vimrc/.config/nvim/plugin_map.vim 	        ~/.config/nvim
-cp $HOME/vimrc/.config/nvim/self_fun.vim 	        ~/.config/nvim
-cp $HOME/vimrc/.config/nvim/user_plugin.vim 	        ~/.config/nvim
-cp $HOME/vimrc/.config/nvim/vi_config.sh  	        ~/.config/nvim
-cp $HOME/vimrc/gtags.conf 			        ~/.config/gtags.conf
-cp $HOME/vimrc/AppData/Local/nvim/coc-settings.json 	~/.config/nvim
-dos2unix ~/.config/nvim/coc-settings.json
+cp $HOME/vimrc/.config/nvim/init.vim 		        $HOME/.config/nvim
+cp $HOME/vimrc/.config/nvim/basic_map.vim 	        $HOME/.config/nvim
+cp $HOME/vimrc/.config/nvim/plugin_config.vim 		$HOME/.config/nvim
+cp $HOME/vimrc/.config/nvim/plugin_map.vim 	        $HOME/.config/nvim
+cp $HOME/vimrc/.config/nvim/self_fun.vim 	        $HOME/.config/nvim
+cp $HOME/vimrc/.config/nvim/user_plugin.vim 	        $HOME/.config/nvim
+cp $HOME/vimrc/.config/nvim/vi_config.sh  	        $HOME/.config/nvim
+chmod 755 $HOME/.config/nvim/vi_config.sh
+cp $HOME/vimrc/gtags.conf 			        $HOME/.config/gtags.conf
+cp $HOME/vimrc/AppData/Local/nvim/coc-settings.json 	$HOME/.config/nvim
+dos2unix $HOME/.config/nvim/coc-settings.json
 
 if [ ! -d $HOME/.config/coc/ultisnips ]; then
     mkdir -p $HOME/.config/coc/ultisnips
@@ -39,6 +40,10 @@ git config --global alias.st "status"
 git config --global alias.br "branch"
 git config --global alias.lo "log --graph --pretty=oneline"
 git config --global alias.co "commit -m"
+
+cat $HOME/.bashrc | grep update_vim
+if [ $? -ne 0 ]
+    echo "alias update_vim='/home/wj/.config/nvim/vi_config.sh'" >> $HOME/.bashrc
 
 #git clone https://github.com/kdurant/bookmark
 
